5d926e01b419ca820b7f9974301597d8d55eed83
jiths3 <jithinchacko71@gmail.com>
2025-01-05 00:34:36 +0530

    setting up tailwind

---
+++ disney-clone/src/Components/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import logo from '../assets/Images/logo.png'
+
+
+function Header() {
+  return (
+    <div>
+        <img src={logo} class="w-[80px] object-cover" />
+      
+    </div>
+  )
+}
+
+export default Header
